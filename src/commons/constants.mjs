c118f354d9a32a2cd2906b73836570aaf80cc72c
zarrok <84244625+zarrok2112@users.noreply.github.com>
2023-09-12 19:58:19 -0500

    constantes y process

---
+++ src/commons/constants.mjs
@@ -0,0 +1,7 @@
+export const FILTERS = {
+    GREYSCALE: 'grayscale',
+    BLUR: 'blur',
+    NEGATIVE: 'negative',
+  };
+  
+export const TYPE_FILTERS = [FILTERS.GREYSCALE, FILTERS.BLUR, FILTERS.NEGATIVE];
---
+++ src/models/process.mjs
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+import { TYPE_FILTERS } from '../commons/constants.mjs';
+
+const ProcessSchema = new Schema(
+  {
+    filters: {
+    type: [
+        {
+          type: String,
+          enum: TYPE_FILTERS,
+          required: true,
+        },
+    ],
+    },
+  },
+  { 
+    timestamps: true 
+}
+);
+const ProcessModel = model('images', ProcessSchema);
+export default ProcessModel;
