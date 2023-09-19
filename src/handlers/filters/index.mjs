20cbcbef3f1c29499520436c0ab7ddc0b75c2fa1
zarrok <84244625+zarrok2112@users.noreply.github.com>
2023-09-19 16:54:34 -0500

    format

--- src/handlers/filters/index.mjs
+++ src/handlers/filters/index.mjs
@@ -1,13 +1,14 @@
 import { Router } from 'express';
+import applyFiltersHandler from './applyFiltersHandler.mjs';
+import upload from '../utils/multerUtils.mjs';
 
 const router = Router();
 
-router.post('/', (req, res) => {
-    res.send('Hello World! POST');
-});
 
-router.get('/', (req, res) => {
-    res.send('Hello World! GET');
-});
+router.get('/', (_, res) => {
+    res.send('aplicacion para filtros');
+  });
+  
+  router.post('/', upload.array('images[]'), applyFiltersHandler);
 
 export default router;
