96f42882bca8e7253f9b73948b4b1fef17b6d97b
zarrok <anakinvv@gmail.com>
2023-09-20 02:14:08 -0500

    add parser filters
    add parser filters
    format all code
    redis creation

--- src/controllers/filters/applyFilters.mjs
+++ src/controllers/filters/applyFilters.mjs
@@ -1,22 +1,26 @@
-import Boom from '@hapi/boom';
-import Joi from 'joi';
-import { FILTERS } from '../../commons/constants.mjs';
+import Process from "../../models/Process.mjs";
+import Boom from "@hapi/boom";
+import Joi from "joi";
+import { FILTERS } from "../../commons/constants.mjs";
 
 const PayloadValidation = Joi.object({
-    filters: Joi.array().min(1).items(
-        Joi.string().valid(FILTERS.NEGATIVE, FILTERS.GREYSCALE, FILTERS.BLUR)
+  filters: Joi.array()
+    .min(1)
+    .items(
+      Joi.string().valid(FILTERS.NEGATIVE, FILTERS.GREYSCALE, FILTERS.BLUR)
     ),
+  files: Joi.array().required(),
 });
 
 const applyFilters = async (payload) => {
-    try {
-        await PayloadValidation.validateAsync(payload);
-    } catch (e) {
-        throw Boom.badData(e.message, { e });
-    }
-    const newProcess = new Process(payload);
-    await newProcess.save();
-    return newProcess;
+  try {
+    await PayloadValidation.validateAsync(payload);
+  } catch (e) {
+    throw Boom.badData(e.message, { e });
+  }
+  const newProcess = new Process(payload);
+  await newProcess.save();
+  return newProcess;
 };
 
 export default applyFilters;
--- src/utils/multerutils.mjs
+++ src/utils/multerutils.mjs
@@ -1,24 +1,22 @@
-import multer from 'multer';
-import Boom from '@hapi/boom';
+import multer from "multer";
+import Boom from "@hapi/boom";
 
-const imagesType = ['image/png'];
+const imagesType = ["image/png"];
 
 const upload = multer({
-    storage: multer.memoryStorage(),
-    fileFilter: (_, file, cb) => {
-        if (imagesType.includes(file.mimetype)) {
-            cb(null, true);
-        } else {
-            const errorMessage = `Solo ${imagesType.join(
-                ', '
-            )} son permitidos`;
-            const error = Boom.badData(errorMessage);
-            cb(error);
-        }
-    },
-    limits: {
-        fileSize: 31457280,
-    },
+  storage: multer.memoryStorage(),
+  fileFilter: (_, file, cb) => {
+    if (imagesType.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      const errorMessage = `Solo ${imagesType.join(", ")} son permitidos`;
+      const error = Boom.badData(errorMessage);
+      cb(error);
+    }
+  },
+  limits: {
+    fileSize: 31457280,
+  },
 });
 
 export default upload;
